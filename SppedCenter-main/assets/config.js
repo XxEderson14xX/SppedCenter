1c35513f2ca918779e34798b548a5f21f630d8ef
XxEderson14xX <65546844+XxEderson14xX@users.noreply.github.com>
2026-09-22 15:33:13 -0600

    Delete SppedCenter-main directory

--- SppedCenter-main/assets/config.js
+++
@@ -1,8 +0,0 @@
-// ============================================================================
-// Configuración de conexión a Supabase
-// ============================================================================
-
-const SUPABASE_URL = "https://cawevglxeaogyokpxovo.supabase.co";
-
-const SUPABASE_ANON_KEY =
-"sb_publishable_PAICA-lvCrKwEbwf9JXVxA_zeMphYJf";
